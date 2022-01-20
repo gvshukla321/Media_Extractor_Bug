f267c018c941a562d0c336d04eb15a05c474a5f9
Gaurav Shukla <gauravshukla@Gauravs-MacBook-Air.local>
2022-01-20 13:21:40 +0530

    initial configuration

---
+++ app/src/main/java/com/comfyapptech/vcut_testing/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.comfyapptech.vcut_testing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.comfyapptech.vcut_testing.ui.theme.VCut_testingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VCut_testingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VCut_testingTheme {
+        Greeting("Android")
+    }
+}
